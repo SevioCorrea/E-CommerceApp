d8ff6b1c9853cb312aeeee9eee657fbb85bc3227
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-06-12 11:12:08 -0400

    First Commit

---
+++ E-CommerceApp/E_CommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceAppApp.swift
+//  E-CommerceApp
+//
+//  Created by Sévio Basilio Corrêa on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
