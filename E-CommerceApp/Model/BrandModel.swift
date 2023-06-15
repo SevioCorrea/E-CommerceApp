cc8a27d2ac6ea6af61132122522bff62e079e2bd
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-06-15 17:24:18 -0400

    BrandGrid

---
+++ E-CommerceApp/Model/BrandModel.swift
@@ -0,0 +1,15 @@
+//
+//  BrandModel.swift
+//  E-CommerceApp
+//
+//  Created by Sévio Basilio Corrêa on 15/06/23.
+//
+
+import Foundation
+
+
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
