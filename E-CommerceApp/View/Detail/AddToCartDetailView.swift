1a5f9f6759f9f26e8f8fb7b7156fb3d0456daa68
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-06-19 09:49:48 -0400

    AddToCart

--- E-CommerceApp/View/Detail/AddToCartDetailView.swift
+++ E-CommerceApp/View/Detail/AddToCartDetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AddToCartDetailView: View {
     // MARK: - Properties
+    @EnvironmentObject var shop: Shop
 
     // MARK: - Body
     var body: some View {
@@ -21,7 +22,7 @@ struct AddToCartDetailView: View {
             Spacer()
         }) //: Button
         .padding(15)
-        .background(Color(red: sampleProduct.red, green: sampleProduct.green, blue: sampleProduct.blue))
+        .background(Color(red:shop.selectedProduct?.red ?? sampleProduct.red, green:shop.selectedProduct?.green ?? sampleProduct.green, blue:shop.selectedProduct?.blue ?? sampleProduct.blue))
         .clipShape(Capsule())
     }
 }
@@ -31,6 +32,7 @@ struct AddToCartDetailView: View {
 struct AddToCartDetailView_Previews: PreviewProvider {
     static var previews: some View {
         AddToCartDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
     }
